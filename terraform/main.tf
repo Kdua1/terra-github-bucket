259cac3ba089963be5eb012acc0af6136f188d10
[Albert Boakye] <[albertbky@gmail.com]>
2023-04-11 21:00:13 -0400

    new changes to set up terrafor

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,9 +7,9 @@ resource "aws_s3_bucket" "terra-github-test" {
   }
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-06e46074ae430fba6" # us-west-2
-  instance_type = "t2.micro"
+# resource "aws_instance" "foo" {
+#   ami           = "ami-06e46074ae430fba6" # us-west-2
+#   instance_type = "t2.micro"
 
 
-}
+# }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "s3" {
+    bucket = "fusion1-state-tf"
+    key    = "fusion1-state-tf"
+    region = "us-east-1"
+  }
 }
 
 # Configure the AWS Provider
